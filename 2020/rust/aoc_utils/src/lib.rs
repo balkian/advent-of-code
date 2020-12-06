ea355f7236bf9be1b83563c91dadbf6c048dad80
J. Fernando Sánchez <balkian@gmail.com>
2020-12-06 16:54:32 +0100

    Refactor aoc_utils and cargo fmt

--- 2020/rust/aoc_utils/src/lib.rs
+++ 2020/rust/aoc_utils/src/lib.rs
@@ -1,71 +1,81 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{BufReader};
+use std::io::BufReader;
 use std::str::FromStr;
 
-use log::{info};
+use log::info;
 
-pub fn file_iter<T>() -> impl Iterator<Item=T> + Send
+pub fn file_iter_parsed<T>() -> impl Iterator<Item = T> + Send
 where
     T: FromStr,
     // If this is missing, the unwrap/expect method after parse() will not compile
     <T as std::str::FromStr>::Err: std::fmt::Debug,
 {
-    file_iter_plain().map(|line| line.parse::<T>().unwrap())
+    file_iter().map(|line| line.parse::<T>().unwrap())
 }
 
-pub fn file_iter_plain() -> impl Iterator<Item=String> + Send {
+pub fn file_iter() -> impl Iterator<Item = String> + Send {
     let args: Vec<String> = env::args().collect();
     let input = &String::from("input.txt");
     let fname = args.get(1).unwrap_or(input);
     info!("Opening file: {}", fname);
     let file = File::open(fname).unwrap();
 
-    BufReader::new(file)
-        .lines()
-        .map(|line| line.unwrap())
-
+    BufReader::new(file).lines().map(|line| line.unwrap())
 }
 
-struct BlockIter<I,M,R,FM,FR>
+///Iterate a file in blocks
+struct BlockIter<I, M, R, FM, FR, FS>
 where
-    I: Iterator<Item=String> + Send,
+    I: Iterator<Item = String> + Send,
     FM: Fn(String) -> M,
     FR: Fn(Vec<M>) -> R,
+    FS: Fn(&String, &Vec<M>) -> bool,
 {
-    inner:  I,
+    inner: I,
     map: FM,
     reduce: FR,
+    split: FS,
 }
 
-pub fn blocks<I>(it: I) -> impl Iterator<Item=String>
+pub fn blocks_default<I>(it: I) -> impl Iterator<Item = String>
 where
-    I: Iterator<Item=String> + Send,
+    I: Iterator<Item = String> + Send,
 {
-    blocks_with_f(it, |x| x, |x| x.join("\n"))
+    blocks(it, |x| x, |x| x.join("\n"), |line, _| line.is_empty())
 }
 
-pub fn blocks_with_f<I,M,R,FM,FR>(inner: I,
-                               map: FM,
-                               reduce: FR) -> impl Iterator<Item=R>
+pub fn blocks<I, M, R, FM, FR, FS>(
+    inner: I,
+    map: FM,
+    reduce: FR,
+    split: FS,
+) -> impl Iterator<Item = R>
 where
-    I: Iterator<Item=String> + Send,
+    I: Iterator<Item = String> + Send,
     FM: Fn(String) -> M,
     FR: Fn(Vec<M>) -> R,
+    FS: Fn(&String, &Vec<M>) -> bool,
 {
-    BlockIter{
+    BlockIter {
         inner,
         map,
         reduce,
+        split,
     }
 }
 
-impl<I,M,R,FM,FR> Iterator for BlockIter<I,M,R,FM,FR>
+pub fn default_split<T>(line: &String, _sofar: &Vec<T>) -> bool {
+    line.is_empty()
+}
+
+impl<I, M, R, FM, FR, FS> Iterator for BlockIter<I, M, R, FM, FR, FS>
 where
-    I: Iterator<Item=String> + Send,
+    I: Iterator<Item = String> + Send,
     FM: Fn(String) -> M,
     FR: Fn(Vec<M>) -> R,
+    FS: Fn(&String, &Vec<M>) -> bool,
 {
     type Item = R;
 
@@ -76,29 +86,29 @@ where
         loop {
             match self.inner.next() {
                 Some(s) => {
-                    if s.is_empty() {
+                    if (self.split)(&s, &sofar) {
                         return Some((self.reduce)(sofar));
                     }
                     sofar.push((self.map)(s));
-                    continue
-                },
+                    continue;
+                }
                 None if sofar.is_empty() => {
                     return None;
-                },
+                }
                 None => {
                     return Some((self.reduce)(sofar));
                 }
             }
         }
-
     }
 }
 
-
-pub fn file_iter_blocks<M,R,FM,FR>(map: FM, reduce: FR) -> impl Iterator<Item=R>
-    where
+///Iterate the input file, process each line with `map`, group lines using `default_split`
+///and aggregate results using `reduce`.
+pub fn file_iter_blocks<M, R, FM, FR>(map: FM, reduce: FR) -> impl Iterator<Item = R>
+where
     FM: Fn(String) -> M,
     FR: Fn(Vec<M>) -> R,
 {
-    blocks_with_f(file_iter_plain(), map, reduce)
+    blocks(file_iter(), map, reduce, default_split)
 }
