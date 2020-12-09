0a09c0c6b3bd3cce01c284529cf1276e04e67b86
J. Fernando Sánchez <balkian@gmail.com>
2020-12-09 11:21:48 +0100

    day9 refactor

--- 2020/rust/day9/src/main.rs
+++ 2020/rust/day9/src/main.rs
@@ -1,48 +1,54 @@
 use std::collections::VecDeque;
 
-const W: usize = 25;
-
 fn main() {
-    let numbers: Vec<usize> = aoc_utils::file_iter().map(|x|
-        x.parse().unwrap()
-    ).collect();
+    let w_size: usize = std::env::args()
+        .nth(2)
+        .map(|x| x.parse().expect("invalid window size"))
+        .unwrap_or(25);
+    let numbers: Vec<usize> = aoc_utils::file_iter()
+        .map(|x| x.parse().expect("could not parse number"))
+        .collect();
 
-    let mut window: VecDeque<usize> = numbers.iter().take(W).map(|x| x.to_owned()).collect();
+    let mut window: VecDeque<usize> = numbers.iter().take(w_size).map(|x| x.to_owned()).collect();
     let mut vuln: Option<&usize> = None;
 
-    for number in &numbers[W..] {
-        // dbg!{&window, &number};
+    for number in &numbers[w_size..] {
         let mut found = false;
         for p1 in &window {
-            if number > p1 && window.contains(&(number-p1)) {
+            if number > p1 && window.contains(&(number - p1)) {
                 found = true;
-                break
+                break;
             }
         }
         if !found {
-            println!{"First number: {:}", number}
             vuln = Some(number);
-            break
+            break;
         }
         window.push_back(*number);
-        if window.len() > W {
+        if window.len() > w_size {
             window.pop_front();
         }
     }
 
+    let vuln = *vuln.expect("vulnerable number not found");
+    println!("Part 1: {:}", vuln);
+
     window = VecDeque::new();
-    let vuln = *vuln.unwrap();
-    let mut sol2:usize = 0;
+    let mut sum = 0;
     for i in numbers {
+        sum += i;
         window.push_back(i);
-        let mut sum: usize = window.iter().sum();
-        while sum > vuln{
-            sum -= window.pop_front().unwrap();
+        while sum > vuln {
+            sum -= window
+                .pop_front()
+                .expect("the sum > vuln but the window is empty?");
         }
         if sum == vuln {
-            sol2 = window.iter().max().unwrap() + window.iter().min().unwrap();
-            break
+            let max = window.iter().max().expect("empty window (impossible?)");
+            let min = window.iter().min().expect("empty window (impossible?)");
+            let sol2 = max + min;
+            println!("Part 2: {:?}", sol2);
+            break;
         }
     }
-    println!("{:?}", sol2);
 }
