5bb447263eca028781d6375c93a12536aef582fe
J. Fernando Sánchez <balkian@gmail.com>
2021-11-12 20:02:30 +0100

    Day 17 refactored

--- 2018/rust/d17/src/main.rs
+++ 2018/rust/d17/src/main.rs
@@ -1,172 +1,164 @@
 use std::cmp::{max, min};
-use std::collections::HashMap;
-use std::{fmt, fs};
+use std::fs;
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
-struct Point(usize, usize);
-
-impl Point {
-    fn down(&self) -> Point {
-        Point(self.0 + 1, self.1)
-    }
-    fn left(&self) -> Point {
-        Point(self.0, self.1 - 1)
-    }
-    fn right(&self) -> Point {
-        Point(self.0, self.1 + 1)
-    }
-    fn up(&self) -> Point {
-        Point(self.0 - 1, self.1)
-    }
+fn left(point: (usize, usize)) -> (usize, usize) {
+    (point.0, point.1 - 1)
+}
+fn right(point: (usize, usize)) -> (usize, usize) {
+    (point.0, point.1 + 1)
+}
+fn down(point: (usize, usize)) -> (usize, usize) {
+    (point.0 + 1, point.1)
+}
+fn up(point: (usize, usize)) -> (usize, usize) {
+    (point.0 - 1, point.1)
 }
 
-#[derive(Debug, Clone, Hash, Copy, PartialEq, Eq)]
-enum Material {
-    Clay,
-    Wet,
-    StillWater,
+const CLAY: char = '#';
+const WET: char = '|';
+const STILL_WATER: char = '~';
+const EMPTY: char = '.';
+
+struct Map {
+    grid: Vec<Vec<char>>,
+    min_x: usize,
+    min_y: usize,
 }
 
-use Material::*;
-
-impl fmt::Display for Material {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let ch = match self {
-            Clay => '#',
-            // Self::StillWater => '~',
-            Wet => '|',
-            StillWater => '~',
-        };
-        write!(f, "{}", ch)
-    }
-}
-
-fn print(points: &HashMap<Point, Material>) {
-    let (min_x, (max_y, max_x)) =
-        points
-            .keys()
-            .fold((usize::MAX, (usize::MIN, usize::MIN)), |acc, point| {
-                (
-                    min(acc.0, point.1),
-                    (max(acc.1 .0, point.0), max(acc.1 .1, point.1)),
-                )
-            });
-    for y in 0..=max_y {
-        for x in min_x..=max_x {
-            if let Some(material) = points.get(&Point(y, x)) {
-                print!("{}", material);
-            } else {
-                print!(".");
+impl Map {
+    fn parse(input: &str) -> Self {
+        let mut points = vec![];
+        for line in input.lines() {
+            let sides: Vec<_> = line.split(", ").collect();
+            let mut values: (Vec<usize>, Vec<usize>) = (vec![], vec![]);
+            for side in sides {
+                let subtokens: Vec<_> = side.split('=').collect();
+                let var = subtokens[0];
+                let mut it = subtokens[1]
+                    .split("..")
+                    .map(|x| x.parse::<usize>().unwrap())
+                    .cycle();
+                let limits = it.next().unwrap()..=it.next().unwrap();
+                match var {
+                    "x" => values.1.extend(limits),
+                    "y" => values.0.extend(limits),
+                    _ => panic!("unknown character"),
+                };
             }
+            for &y in &values.0 {
+                for &x in &values.1 {
+                    points.push((y, x));
+                }
+            }
+        }
+        let (min_y, min_x, max_y, max_x) = points.iter().fold(
+            (usize::MAX, usize::MAX, usize::MIN, usize::MIN),
+            |acc, point| {
+                (
+                    min(acc.0, point.0),
+                    min(acc.1, point.1),
+                    max(acc.2, point.0),
+                    max(acc.3, point.1),
+                )
+            },
+        );
+        let min_x = min(500, min_x - 1);
+        let min_y = min(0, min_y - 1);
+        let max_x = max_x + 1;
+        let max_y = max_y + 1;
+
+        let mut grid = vec![vec!(EMPTY; (max_x - min_x) + 1); (max_y - min_y) + 1];
+        for point in points {
+            grid[point.0 - min_y][point.1 - min_x] = CLAY;
+        }
+        Map { grid, min_x, min_y }
+    }
+    fn flow(&mut self) {
+        let points = &mut self.grid;
+        let mut sources = vec![(1 - self.min_y, 500 - self.min_x)];
+        while let Some(pos) = sources.pop() {
+            if pos.0 == points.len() - 2 {
+                continue;
+            }
+
+            if pos.0 >= points.len() || (points[pos.0][pos.1] != EMPTY) {
+                continue;
+            }
+
+            let d = down(pos);
+
+            match points[d.0][d.1] {
+                EMPTY => {
+                    points[pos.0][pos.1] = WET;
+                    sources.push(d);
+                    continue;
+                }
+                WET => {
+                    points[pos.0][pos.1] = WET;
+                    continue;
+                }
+                _ => {}
+            }
+
+            let mut has_drain = false;
+
+            let mut line = vec![pos];
+            for func in [left, right] {
+                let mut newpos = pos;
+                for _ in 0.. {
+                    newpos = func(newpos);
+                    if points[newpos.0][newpos.1] == CLAY {
+                        break;
+                    }
+                    line.push(newpos);
+                    let d = down(newpos);
+                    if points[d.0][d.1] == EMPTY {
+                        sources.push(d);
+                        has_drain = true;
+                        break;
+                    }
+                }
+            }
+            let mat = if has_drain {
+                WET
+            } else {
+                let u = up(pos);
+                points[u.0][u.1] = EMPTY;
+                sources.push(u);
+                STILL_WATER
+            };
+            for point in line {
+                points[point.0][point.1] = mat;
+            }
+        }
+    }
+    fn print(&self) {
+        let points = &self.grid;
+        for row in points.iter() {
+            for x in row.iter() {
+                print!("{}", x);
+            }
+            println!();
         }
         println!();
     }
-    println!();
-}
-
-fn flow(points: &mut HashMap<Point, Material>) {
-    let ((min_y, _min_x), (max_y, _max_x)) = points.keys().fold(
-        ((usize::MAX, usize::MAX), (usize::MIN, usize::MIN)),
-        |acc, point| {
-            (
-                (min(acc.0 .1, point.0), min(acc.0 .0, point.1)),
-                (max(acc.1 .0, point.0), max(acc.1 .1, point.1)),
-            )
-        },
-    );
-    let mut sources = vec![Point(min_y - 1, 500)];
-    while let Some(pos) = sources.pop() {
-        if pos.0 == max_y {
-            println!("Reached end");
-            continue;
-        }
-        if sources.contains(&pos) {
-            continue;
-        }
-
-        let (left, right, down) = (pos.left(), pos.right(), pos.down());
-
-        match points.get(&down) {
-            None => {
-                points.insert(down.clone(), Wet);
-                sources.push(down);
-                continue;
-            }
-            Some(Wet) => {
-                points.insert(pos.clone(), Wet);
-                continue;
-            }
-            _ => {}
-        }
-
-        let mut has_drain = false;
-
-        let mut line = vec![pos.clone()];
-        for func in [Point::left, Point::right] {
-            let mut newpos = pos.clone();
-            for delta in 0.. {
-                newpos = func(&newpos);
-                if points.get(&newpos) == Some(&Clay) {
-                    break;
-                }
-                line.push(newpos.clone());
-                if points.get(&newpos.down()).is_none() {
-                    sources.push(newpos.clone());
-                    has_drain = true;
-                    break;
-                }
-            }
-        }
-        let mat = if has_drain {
-            Wet
-        } else {
-            sources.push(pos.up());
-            StillWater
-        };
-        for point in line {
-            points.insert(point, mat);
-        }
-    }
-}
-
-fn parse(input: &str) -> HashMap<Point, Material> {
-    let mut points = HashMap::new();
-    for line in input.lines() {
-        let sides: Vec<_> = line.split(", ").collect();
-        let mut values: (Vec<usize>, Vec<usize>) = (vec![], vec![]);
-        for side in sides {
-            let subtokens: Vec<_> = side.split("=").collect();
-            let var = subtokens[0];
-            let mut it = subtokens[1]
-                .split("..")
-                .map(|x| x.parse::<usize>().unwrap())
-                .cycle();
-            let limits = it.next().unwrap()..=it.next().unwrap();
-            match var {
-                "x" => values.1.extend(limits),
-                "y" => values.0.extend(limits),
-                _ => panic!("unknown character"),
-            };
-        }
-        for y in &values.0 {
-            for x in &values.1 {
-                points.insert(Point(*y, *x), Material::Clay);
-            }
-        }
-    }
-    points
 }
 
 fn solve1(input: &str) -> (usize, usize) {
-    let mut points = parse(input);
-    flow(&mut points);
-    print(&points);
+    let mut map = Map::parse(input);
+    map.flow();
+    map.print();
     (
-        points
-            .values()
-            .filter(|v| matches!(v, StillWater | Wet))
+        map.grid
+            .iter()
+            .flatten()
+            .filter(|&v| matches!(*v, STILL_WATER | WET))
             .count(),
-        points.values().filter(|v| matches!(v, StillWater)).count(),
+        map.grid
+            .iter()
+            .flatten()
+            .filter(|&v| matches!(*v, STILL_WATER))
+            .count(),
     )
 }
 
