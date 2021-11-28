51bd435f0f1039ad18db20dcb597f9fba957befd
J. Fernando Sánchez <balkian@gmail.com>
2021-11-28 17:20:47 +0100

    2015 rust day04

---
+++ 2015/rust/src/day04.rs
@@ -0,0 +1,40 @@
+use crate::aoc_test;
+use md5;
+
+pub fn parse(input: &str) -> &str {
+    input
+}
+
+fn is_valid(input: &str, i: usize, difficulty: usize) -> bool {
+    let st = input.to_string() + &i.to_string();
+    let digest = md5::compute(st.as_bytes());
+    let bytes = difficulty / 2;
+
+    for i in 0..bytes {
+        if digest[i] != 0 {
+            // dbg!(i, digest[i]);
+            return false
+        }
+    }
+    if (difficulty % 2 == 1) && (digest[bytes] & 0xF0) != 0 {
+        return false
+    }
+    return true
+}
+
+pub fn part1(input: &str) -> usize {
+    (0..).into_iter().find(|i| is_valid(input, *i, 5)).unwrap()
+}
+
+pub fn part2(input: &str) -> usize {
+    (0..).into_iter().find(|i| is_valid(input, *i, 6)).unwrap()
+}
+
+#[test]
+fn test_valid(){
+    assert!(is_valid("abcdef", 609043, 5));
+
+}
+
+aoc_test!(part1, abc, "abcdef", 609043);
+aoc_test!(part1, pqr, "pqrstuv", 1048970);
---
+++ 2015/rust/src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+
+macro_rules! aoc_main {
+
+    ($($day:ident)*) => {
+        $(mod $day;)*
+
+        fn main() {
+            match env::args().nth(1) {
+                $( Some($day) => {
+
+                    let fname = stringify!($day.input);
+                    let input = &std::fs::read_to_string(fname).expect("could not read input file");
+                    let input = $day::parse(input);
+                    println!("Part 1 {}", $day::part1(input));
+                    println!("Part 2 {}", $day::part2(input));
+                },)* 
+                _ => panic!("not implemented"),
+            }
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! aoc_test {
+    ($part:ident, $name:ident, $input:tt, $expected:expr) => {
+        #[test]
+        fn $name () {
+            assert_eq!($part($input), $expected);
+        }
+
+    }
+}
+
+aoc_main!(day04);
