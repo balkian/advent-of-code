98d644f05a68e848c72ea789ae7e552cf7e6eab9
J. Fernando Sánchez <balkian@gmail.com>
2021-12-06 19:09:43 +0100

    2021 rust day06 slight refactor

--- 2021/rust/src/solutions/day06.rs
+++ 2021/rust/src/solutions/day06.rs
@@ -7,20 +7,18 @@ pub fn part1(input: &[usize]) -> usize {
 pub fn part2(input: &[usize]) -> usize {
     part1_times(input, 256)
 }
+
 pub fn part1_times(input: &[usize], times: usize) -> usize {
     let mut fish: HashMap<usize, usize> = HashMap::new();
-    for age in input {
-        *fish.entry(*age).or_insert(0) += 1;
+    for day in input {
+        *fish.entry(*day).or_insert(0) += 1;
     }
 
     for _ in 0..times {
         let mut nufish: HashMap<usize, usize> = HashMap::new();
         for day in fish.keys() {
-            if day > &0 {
-                *nufish.entry(day - 1).or_insert(0) += *fish.get(day).unwrap();
-            } else {
-                *nufish.entry(6).or_insert(0) += *fish.get(day).unwrap();
-            }
+            let next_day = if day > &0 { day - 1 } else { 6 };
+            *nufish.entry(next_day).or_insert(0) += *fish.get(day).unwrap();
         }
         nufish.insert(8, *fish.get(&0).unwrap_or(&0));
         fish = nufish;
