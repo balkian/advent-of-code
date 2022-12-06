523069bef2988b07a6af2191ac899d13d58b910c
J. Fernando Sánchez <balkian@gmail.com>
2022-12-06 09:12:05 +0100

    2022 rust day06

---
+++ 2022/rust/src/solutions/day06.rs
@@ -0,0 +1,23 @@
+use itertools::Itertools;
+pub fn parse(input: &str) -> &str {
+    input.trim()
+}
+
+pub fn solve(input: &str, num: usize) -> usize {
+    input
+        .as_bytes()
+        .windows(num)
+        .enumerate()
+        .find(|(_idx, win)| win.iter().duplicates().next().is_none())
+        .unwrap()
+        .0
+        + num
+}
+
+pub fn part1(input: &str) -> usize {
+    solve(input, 4)
+}
+
+pub fn part2(input: &str) -> usize {
+    solve(input, 14)
+}
