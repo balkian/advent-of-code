e75ce6b295830e8060bbce2bb0de8f0ce9432a98
J. Fernando Sánchez <balkian@gmail.com>
2022-12-22 17:58:13 +0100

    2022 rust day21

--- 2022/rust/src/solutions/day21.rs
+++ 2022/rust/src/solutions/day21.rs
@@ -1,78 +1,185 @@
-use std::collections::HashMap;
+/// What a mess! Next time I should use a proper graph/tree library
+use std::collections::{HashMap, VecDeque};
+use std::ops::{Add, Div, Mul, Sub};
 
-type Input<'a> = HashMap<&'a str, Rule<'a>>;
-
-pub enum Rule<'a> {
-    Number(isize),
-    Op(&'a str, &'a str, &'a str),
+#[derive(Debug, Clone)]
+pub struct TreeDef<'a> {
+    out: HashMap<&'a str, Node>,
+    stack: VecDeque<(&'a str, &'a str, &'a str, &'a str)>,
 }
 
-impl<'a> Rule<'a> {
-    fn value(
-        &self,
-        calculated: &HashMap<&'a str, isize>,
-    ) -> Result<isize, (Option<&'a str>, Option<&'a str>)> {
-        match self {
-            Rule::Number(i) => Ok(*i),
-            Rule::Op(op, a, b) => match (calculated.get(a), calculated.get(b)) {
-                (Some(a), Some(b)) => {
-                    let res = match *op {
-                        "*" => a * b,
-                        "+" => a + b,
-                        "-" => a - b,
-
-                        "/" => a / b,
-                        _ => panic!("unsupported operation {op}"),
+impl<'a> TreeDef<'a> {
+    fn get_root(&self) -> Node {
+        let mut out = self.out.clone();
+        let mut stack = self.stack.clone();
+        while let Some((name, op1, op, op2)) = stack.pop_front() {
+            if out.contains_key(op1) && out.contains_key(op2) {
+                if let (Some(op1), Some(op2)) = (out.get(&op1), out.get(&op2)) {
+                    let newnode = match op {
+                        "*" => op1 * op2,
+                        "-" => op1 - op2,
+                        "+" => op1 + op2,
+                        "/" => op1 / op2,
+                        "=" => Node::Op(Op::Eq, Box::new(op1.clone()), Box::new(op2.clone())),
+                        t => panic!("unknown operation {t}"),
                     };
-                    Ok(res)
+                    out.insert(name, newnode);
                 }
-                (Some(_), None) => Err((None, Some(b))),
-                (None, None) => Err((Some(a), Some(b))),
-                (None, Some(_)) => Err((Some(a), None)),
-            },
+            } else {
+                stack.push_back((name, op1, op, op2));
+            }
+        }
+
+        out.remove("root").unwrap()
+    }
+}
+
+type NodeRef = Box<Node>;
+
+#[derive(Debug, Copy, Clone)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Eq,
+}
+
+impl Op {
+    fn apply(&self, a: &i128, b: &i128) -> Option<i128> {
+        match self {
+            Op::Add => Some(a + b),
+            Op::Sub => Some(a - b),
+            Op::Mul => Some(a * b),
+            Op::Div => {
+                if a % b == 0 {
+                    Some(a / b)
+                } else {
+                    None
+                }
+            }
+            Op::Eq => panic!("this should never be applied"),
         }
     }
 }
 
-pub fn parse(input: &str) -> Input {
-    let mut out: Input = Default::default();
+#[derive(Debug, Clone)]
+pub enum Node {
+    Op(Op, NodeRef, NodeRef),
+    Num(i128),
+    Human,
+}
+
+impl Node {
+    fn simplify(&self) -> Option<Self> {
+        let res = match self {
+            Node::Op(op, a, b) => match (a.simplify(), b.simplify()) {
+                (Some(Node::Num(a)), Some(Node::Num(b))) => Node::Num(op.apply(&a, &b)?),
+                (Some(a), Some(b)) => Node::Op(*op, Box::new(a), Box::new(b)),
+                (Some(a), None) => Node::Op(*op, Box::new(a), b.clone()),
+                (None, Some(b)) => Node::Op(*op, a.clone(), Box::new(b)),
+                _ => return None,
+            },
+            _ => self.clone(),
+        };
+        Some(res)
+    }
+
+    fn balance(&self) -> Option<Self> {
+        match self {
+            Node::Op(Op::Eq, a, b) => match (a.as_ref(), b.as_ref()) {
+                (a @ &Node::Num(i), Node::Op(op, b, c))
+                | (Node::Op(op, b, c), a @ &Node::Num(i)) => {
+                    let (left, right) = match (op, b.value(), c.value()) {
+                        //     (&Node::Num(b), c) | (c, &Node::Num(b)) => {
+                        (Op::Add, Some(_), None) => (c.clone(), a - b),
+                        (Op::Add, None, Some(_)) => (b.clone(), a - c),
+                        (Op::Sub, Some(_), None) => (c.clone(), &**b - a),
+                        (Op::Sub, None, Some(_)) => (b.clone(), a + c),
+                        (Op::Mul, None, Some(j)) if i % j == 0 => (b.clone(), a / c),
+                        (Op::Mul, Some(j), None) if i % j == 0 => (c.clone(), a / b),
+                        (Op::Div, Some(j), None) if j % i == 0_ => (c.clone(), &(**b).clone() / a),
+                        (Op::Div, None, Some(_)) => (b.clone(), a * c),
+                        _ => todo!(),
+                    };
+                    return Some(Node::Op(
+                        Op::Eq,
+                        Box::new(left.simplify().unwrap()),
+                        Box::new(right.simplify().unwrap()),
+                    ));
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+        None
+    }
+
+    fn value(&self) -> Option<i128> {
+        match self {
+            Node::Num(i) => Some(*i),
+            _ => None,
+        }
+    }
+}
+
+macro_rules! impl_op {
+    ($key:ident, $func:ident) => {
+        impl $key for &Node {
+            type Output = Node;
+
+            fn $func(self, other: Self) -> Self::Output {
+                Node::Op(Op::$key, Box::new(self.clone()), Box::new(other.clone()))
+            }
+        }
+    };
+}
+
+impl_op!(Add, add);
+impl_op!(Mul, mul);
+impl_op!(Sub, sub);
+impl_op!(Div, div);
+
+pub fn parse(input: &str) -> TreeDef {
+    let mut out: HashMap<&str, Node> = Default::default();
+    let mut stack: VecDeque<(&str, &str, &str, &str)> = Default::default();
     for line in input.lines().filter(|l| !l.is_empty()) {
         let (l, r) = line.trim().split_once(": ").unwrap();
-        if let Ok(num) = r.trim().parse::<isize>() {
-            out.insert(l, Rule::Number(num));
+        if let Ok(num) = r.trim().parse::<i128>() {
+            out.insert(l, Node::Num(num));
         } else {
             let toks: Vec<_> = r.trim().split_whitespace().collect();
-            out.insert(l, Rule::Op(toks[1], toks[0], toks[2]));
+            stack.push_front((l, toks[0], toks[1], toks[2]));
         }
     }
-    out
+    TreeDef { out, stack }
 }
 
-pub fn part1(input: &Input) -> isize {
-    let mut stack = vec![];
-    let mut calculated: HashMap<&str, isize> = Default::default();
-    let root = "root";
-    stack.push(root.clone());
-    while !stack.is_empty() {
-        let key = stack.last().unwrap();
-        match input[key].value(&calculated) {
-            Ok(i) => {
-                let key = stack.pop().unwrap();
-                calculated.insert(key, i);
-            }
-            Err((a, b)) => {
-                if let Some(a) = a {
-                    stack.push(a);
-                }
-                if let Some(b) = b {
-                    stack.push(b);
-                }
-            }
+pub fn part1(input: &TreeDef) -> i128 {
+    let root = input.get_root().simplify().unwrap();
+    root.value().unwrap()
+}
+
+pub fn part2(input: &TreeDef) -> i128 {
+    let mut input = input.clone();
+    input.out.insert("humn", Node::Human);
+    input.stack.iter_mut().for_each(|(name, _op1, op, _op2)| {
+        if *name == "root" {
+            *op = "="
         }
+    });
+    let mut root = input.get_root().simplify().unwrap();
+    while let Some(new_root) = root.balance() {
+        root = new_root;
     }
-    calculated[root]
-}
-
-pub fn part2(input: &Input) -> isize {
-    todo!();
+    match root {
+        Node::Op(Op::Eq, a, b) => match (*a, *b) {
+            (num, Node::Human) | (Node::Human, num) => {
+                return num.value().unwrap();
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+    panic!("no human on either side");
 }
