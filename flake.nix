734091484b552c0ad0426948c686eb593187ef0b
J. Fernando Sánchez <balkian@gmail.com>
2023-12-01 19:35:09 +0100

    2023: nixify all the things

--- flake.nix
+++ flake.nix
@@ -1,69 +1,46 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        # flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
-    # rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils,fenix }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default =
-        let
-          toolchain = fenix.packages.${system}.minimal.toolchain;
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
+  outputs = { self, crane, nixpkgs, flake-utils,fenix }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        craneLib = crane.lib.${system}.overrideToolchain
+          fenix.packages.${system}.minimal.toolchain;
+        pkgs = nixpkgs.legacyPackages.${system};
+        my-crate = craneLib.buildPackage {
+            src = ./. ;
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+            doCheck = false;
+          };
 
-        (pkgs.makeRustPlatform {
-          cargo = toolchain;
-          rustc = toolchain;
-        }).buildRustPackage {
-          pname = "example";
-          version = "0.1.0";
 
-          src = ./2023/rust/;
+      in
+      {
+      checks = { inherit my-crate; };
+      packages.default = my-crate;
+      devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
 
-          cargoLock.lockFile = ./2023/rust/Cargo.lock;
+          # Extra inputs can be added here; cargo and rustc are provided by default
+          # from the toolchain that was specified earlier.
+          packages = [
+            # rustWithWasiTarget
+          ];
         };
     });
-    #   let
-    #   pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    #   fooScript = pkgs.writeScriptBin "foo.sh" ''
-    #     #!/bin/sh
-    #     echo $FOO
-    #   '';
-    # in {
-    #   devShells.x86_64-linux.default = pkgs.mkShell {
-    #     name = "AoC build environment";
-    #     buildInputs = [
-    #       pkgs.python39
-    #       pkgs.python39Packages.tox
-    #       pkgs.python39Packages.flake8
-    #       pkgs.python39Packages.requests
-    #       pkgs.python39Packages.ipython
-    #       fooScript
-    #     ];
-    #     shellHook = ''
-    #       echo "Welcome in $name"
-    #       export FOO="BAR"
-    #     '';
-    #   };
-    # };
-    # flake-utils.lib.eachDefaultSystem
-    #   (system:
-    #     let
-    #       overlays = [ (import fenix) ];
-    #       pkgs = import nixpkgs {
-    #         inherit system overlays;
-    #       };
-    #     in
-    #     with pkgs;
-    #     {
-    #       devShells.default = mkShell {
-    #         buildInputs = [ rust-bin.stable.latest.default ];
-    #       };
-    #     }
-    #   );
 }
