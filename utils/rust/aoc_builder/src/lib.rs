a2e98d721fb03f806763c579fe68a07ac1500865
J. Fernando Sánchez <balkian@gmail.com>
2023-12-11 11:44:13 +0100

    utils: build.rs correct fmt output main&solutions

--- utils/rust/aoc_builder/src/lib.rs
+++ utils/rust/aoc_builder/src/lib.rs
@@ -9,7 +9,7 @@ pub fn main() {
     let dest_path = Path::new("src").join("main.rs");
     let mut f = File::create(&dest_path).unwrap();
     f.write_all(b"mod solutions;\n\n").unwrap();
-    f.write_all(b"fn main() { solutions::main(); }").unwrap();
+    f.write_all(b"fn main() {\n    solutions::main();\n}\n").unwrap();
 
     let dest_path = Path::new("src").join("solutions.rs");
     let mut f = File::create(&dest_path).unwrap();
@@ -36,5 +36,5 @@ pub fn main() {
     for mod_name in mods.into_iter() {
         f.write_all(format!("{};", mod_name).as_bytes()).unwrap();
     }
-    f.write_all(b");").unwrap();
+    f.write_all(b");\n").unwrap();
 }
