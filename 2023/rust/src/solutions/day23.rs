53b619cf9e494ba025ee1a08352b9ea903a2ddd7
J. Fernando Sánchez <balkian@gmail.com>
2024-05-15 12:30:55 +0200

    2023 rust day23 fmt clippy

--- 2023/rust/src/solutions/day23.rs
+++ 2023/rust/src/solutions/day23.rs
@@ -1,15 +1,15 @@
-use std::collections::{HashMap,HashSet};
+use std::collections::{HashMap, HashSet};
 
 type Point = (usize, usize);
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 struct Path {
     last: (usize, usize),
     previous: HashSet<(usize, usize)>,
     distance: usize,
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 pub struct Grid {
     tiles: Vec<Vec<char>>,
     graph: HashMap<Point, HashMap<Point, usize>>,
@@ -18,7 +18,7 @@ pub struct Grid {
 impl Grid {
     fn new(tiles: Vec<Vec<char>>) -> Self {
         let graph = Default::default();
-        let mut grid = Self{tiles, graph};
+        let mut grid = Self { tiles, graph };
         let mut new_intersections = vec![];
         new_intersections.push(grid.start());
         let end = grid.end();
@@ -47,55 +47,68 @@ impl Grid {
                     new_intersections.push(n);
                 } else {
                     for opt in opts {
-                        pending.push((opt, dist+1));
+                        pending.push((opt, dist + 1));
                     }
                 }
             }
-
         }
         grid
-    } 
-
-    fn options_from_graph(&self, pos: (usize, usize)) -> impl Iterator<Item = (&Point, &usize)> {
-        self.graph.get(&pos)
-            .expect("origin not in graph")
-            .into_iter()
     }
 
-    fn options_from_tiles(&self, pos: (usize, usize), slippery: bool) -> Vec<(usize, usize)>{
+    fn options_from_graph(&self, pos: (usize, usize)) -> impl Iterator<Item = (&Point, &usize)> {
+        self.graph
+            .get(&pos)
+            .expect("origin not in graph")
+            .iter()
+    }
+
+    fn options_from_tiles(&self, pos: (usize, usize), slippery: bool) -> Vec<(usize, usize)> {
         let mut next = match (slippery, self.tiles[pos.0][pos.1]) {
             (_, '#') => vec![],
-            (true, '>') => vec![(pos.0, pos.1+1)],
-            (true, '<') => vec![(pos.0, pos.1-1)],
-            (true, 'v') => vec![(pos.0+1, pos.1)],
-            (true, '^') => vec![(pos.0-1, pos.1)],
-            (_, '.') | (false, 'v' | '^' | '>' | '<') => vec![(pos.0, pos.1+1),
-                        (pos.0, pos.1-1),
-                        (pos.0+1, pos.1),
-                        (pos.0-1, pos.1)],
+            (true, '>') => vec![(pos.0, pos.1 + 1)],
+            (true, '<') => vec![(pos.0, pos.1 - 1)],
+            (true, 'v') => vec![(pos.0 + 1, pos.1)],
+            (true, '^') => vec![(pos.0 - 1, pos.1)],
+            (_, '.') | (false, 'v' | '^' | '>' | '<') => vec![
+                (pos.0, pos.1 + 1),
+                (pos.0, pos.1 - 1),
+                (pos.0 + 1, pos.1),
+                (pos.0 - 1, pos.1),
+            ],
             _ => panic!("unknown char"),
         };
         next.retain(|pos| {
             pos.0 > 0 && pos.1 > 0 && pos.0 < self.tiles.len() && self.tiles[pos.0][pos.1] != '#'
-
         });
         next
     }
 
     fn start(&self) -> (usize, usize) {
-        (1, self.tiles[0].iter().position(|&t| t == '.').expect("start not found"))
-
+        (
+            1,
+            self.tiles[0]
+                .iter()
+                .position(|&t| t == '.')
+                .expect("start not found"),
+        )
     }
     fn end(&self) -> (usize, usize) {
-        (self.tiles.len()-2, self.tiles[self.tiles.len()-1].iter().position(|&t| t == '.').expect("end not found"))
-
+        (
+            self.tiles.len() - 2,
+            self.tiles[self.tiles.len() - 1]
+                .iter()
+                .position(|&t| t == '.')
+                .expect("end not found"),
+        )
     }
 }
 
 pub fn parse(input: &str) -> Grid {
-    let tiles = input.lines().filter(|line| !line.is_empty()).map(|line| {
-        line.chars().collect()
-    }).collect();
+    let tiles = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.chars().collect())
+        .collect();
     Grid::new(tiles)
 }
 
@@ -103,8 +116,12 @@ pub fn part1(input: &Grid) -> usize {
     let s = input.start();
     let e = input.end();
     let mut paths: Vec<Path> = vec![];
-    paths.push(Path{last: s, previous: Default::default(), distance: 1});
-    let mut done : Vec<Path> = Default::default();
+    paths.push(Path {
+        last: s,
+        previous: Default::default(),
+        distance: 1,
+    });
+    let mut done: Vec<Path> = Default::default();
     while let Some(last) = paths.pop() {
         for opt in input.options_from_tiles(last.last, true) {
             if last.previous.contains(&opt) {
@@ -128,7 +145,11 @@ pub fn part2(input: &Grid) -> usize {
     let s = input.start();
     let e = input.end();
     let mut paths: Vec<Path> = vec![];
-    paths.push(Path{last: s, previous: Default::default(), distance: 1});
+    paths.push(Path {
+        last: s,
+        previous: Default::default(),
+        distance: 1,
+    });
     let mut max = usize::MIN;
     while let Some(last) = paths.pop() {
         for (opt, dist) in input.options_from_graph(last.last) {
