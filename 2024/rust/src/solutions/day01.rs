7b4588eb15a7dec760e17ff4594671740fafb44e
J. Fernando Sánchez <balkian@gmail.com>
2024-12-01 10:54:18 +0100

    2024 rust: day01 part2

--- 2024/rust/src/solutions/day01.rs
+++ 2024/rust/src/solutions/day01.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::str::FromStr;
 
 pub fn parse(input: &str) -> Vec<(usize, usize)> {
@@ -5,18 +6,44 @@ pub fn parse(input: &str) -> Vec<(usize, usize)> {
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| {
-            let nums: Vec<usize> = line.trim().split_whitespace().map(|num| usize::from_str(num).expect("invalid number")).collect();
+            let nums: Vec<usize> = line
+                .trim()
+                .split_whitespace()
+                .map(|num| usize::from_str(num).expect("invalid number"))
+                .collect();
             (nums[0], nums[1])
-        }).collect()
+        })
+        .collect()
 }
 
 pub fn part1(pairs: &[(usize, usize)]) -> usize {
-    let (mut num1, mut num2) = pairs.iter().fold((vec![], vec![]), |(mut l1, mut l2), (n1, n2)| { l1.push(n1); l2.push(n2); (l1, l2)});
+    let (mut num1, mut num2) = pairs
+        .iter()
+        .fold((vec![], vec![]), |(mut l1, mut l2), (n1, n2)| {
+            l1.push(n1);
+            l2.push(n2);
+            (l1, l2)
+        });
     num1.sort();
     num2.sort();
-    num1.iter().zip(num2.iter()).fold(0, |acc, (&n1, &n2)| { acc + (n1.abs_diff(*n2))})
+    num1.iter()
+        .zip(num2.iter())
+        .fold(0, |acc, (&n1, &n2)| acc + n1.abs_diff(*n2))
 }
 
 pub fn part2(pairs: &[(usize, usize)]) -> usize {
-    todo!()
+    let (count1, count2) = pairs.iter().fold(
+        (
+            HashMap::<usize, usize>::new(),
+            HashMap::<usize, usize>::new(),
+        ),
+        |(mut c1, mut c2), (n1, n2)| {
+            *c1.entry(*n1).or_default() += 1;
+            *c2.entry(*n2).or_default() += 1;
+            (c1, c2)
+        },
+    );
+    count1.into_iter().fold(0, |acc, (n, c)| {
+        acc + n * c * count2.get(&n).copied().unwrap_or_default()
+    })
 }
