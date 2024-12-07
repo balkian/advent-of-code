7f4479ff74d1fe8cf16d1264492460a506f81b07
J. Fernando Sánchez <balkian@gmail.com>
2024-12-07 09:38:42 +0100

    2024 rust: day07 rustfmt

--- 2024/rust/src/solutions/day07.rs
+++ 2024/rust/src/solutions/day07.rs
@@ -1,17 +1,16 @@
-use nom::character::complete::line_ending;
-use nom::multi::separated_list1;
-use nom::combinator::map;
-use nom::character::complete::i64 as nomi64;
-use nom::character::complete::{space1, multispace1};
-use nom::sequence::{terminated, separated_pair};
-use nom::bytes::complete::tag;
-use nom::IResult;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{i64 as nomi64, line_ending, multispace1, space1},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{separated_pair, terminated},
+    IResult,
+};
 
 #[derive(Debug, Clone)]
 pub struct Equation {
     res: i64,
     ops: Vec<i64>,
-
 }
 
 impl Equation {
@@ -19,12 +18,12 @@ impl Equation {
         let mut res = vec![self.ops[0]];
         for op in self.ops[1..].iter() {
             let mut newres = vec![];
-            for r in res.drain(..){
+            for r in res.drain(..) {
                 newres.push(r * op);
                 newres.push(r + op);
                 if concat {
                     let digits = op.ilog10() + 1;
-                    newres.push(r * 10i64.pow(digits)  + op);
+                    newres.push(r * 10i64.pow(digits) + op);
                 }
             }
             res = newres;
@@ -35,7 +34,10 @@ impl Equation {
 }
 
 pub fn parseall(i: &str) -> IResult<&str, Vec<Equation>> {
-    let equation = map(separated_pair(nomi64, tag(": "), separated_list1(space1, nomi64)), |(res, ops)| Equation{res, ops});
+    let equation = map(
+        separated_pair(nomi64, tag(": "), separated_list1(space1, nomi64)),
+        |(res, ops)| Equation { res, ops },
+    );
     terminated(separated_list1(line_ending, equation), multispace1)(i)
 }
 
@@ -46,13 +48,12 @@ pub fn parse(i: &str) -> Vec<Equation> {
 }
 
 pub fn part1(eqs: &[Equation]) -> i64 {
-    eqs.iter().filter(|e| {
-        e.is_valid(false)
-    }).map(|e| e.res).sum()
+    eqs.iter()
+        .filter(|e| e.is_valid(false))
+        .map(|e| e.res)
+        .sum()
 }
 
 pub fn part2(eqs: &[Equation]) -> i64 {
-    eqs.iter().filter(|e| {
-        e.is_valid(true)
-    }).map(|e| e.res).sum()
+    eqs.iter().filter(|e| e.is_valid(true)).map(|e| e.res).sum()
 }
