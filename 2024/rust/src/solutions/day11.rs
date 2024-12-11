dabb055d9dee32a5e9678b7db5e719b92ea43be3
J. Fernando Sánchez <balkian@gmail.com>
2024-12-11 11:44:29 +0100

    vim

--- 2024/rust/src/solutions/day11.rs
+++ 2024/rust/src/solutions/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Clone, Debug)]
 pub struct Node {
     value: usize,
@@ -30,7 +32,6 @@ impl Iterator for NodeIterator {
     }
 }
 
-
 impl Node {
     fn blink(&mut self) -> &mut Self  {
         if self.value == 0 {
@@ -41,26 +42,24 @@ impl Node {
         if n_digits % 2 == 0 {
             let mult = 10usize.pow(n_digits / 2);
             let tail = self.next.replace(Box::new(Node{value: self.value % mult, next: None}));
-            self.value = self.value / mult;
-            let mut right = self.next.as_mut().unwrap();
+            self.value /= mult;
+            let right = self.next.as_mut().unwrap();
             right.next = tail;
             right
         } else {
-            self.value = self.value * 2024;
-            return self;
+            self.value *= 2024;
+            self
         }
     }
 
 }
 
-pub fn parse(i: &str) -> Node {
-    let mut nodes = i.split_whitespace().map(|n| Node{value: n.parse::<usize>().expect("could not parse number"), next: None}).rev();
-    let last = nodes.next().expect("no last node");
-    nodes.fold(last, |acc, mut node| {node.next = Some(Box::new(acc)); node})
+pub fn parse(i: &str) -> Vec<usize> {
+    i.split_whitespace().map(|n| n.parse::<usize>().expect("could not parse number")).collect()
 }
 
-pub fn part1(i: &Node) -> usize {
-    let mut root = i.clone();
+pub fn part1(i: &[usize]) -> usize {
+    let mut root = i.iter().rev().fold(None, |acc, val| Some(Node{value: *val, next: acc.map(Box::new)})).expect("no nodes");
     for _i in 0..25 {
         let mut cur = &mut root;
         loop {
@@ -74,6 +73,27 @@ pub fn part1(i: &Node) -> usize {
     }
     root.into_iter().count()
 }
-pub fn part2(i: &Node) -> usize {
-    todo!();
+pub fn part2(i: &[usize]) -> usize {
+    let mut counter: HashMap<usize, usize> = i.iter().fold(Default::default(), |mut acc, val| {*acc.entry(*val).or_default() += 1; acc});
+    for _i in 0..75 {
+        let mut nc: HashMap<usize, usize> = Default::default();
+        for (val, count) in counter {
+            if val == 0 {
+                *nc.entry(1).or_default() += count;
+                continue;
+            }
+            let n_digits = val.ilog10() + 1;
+            if n_digits % 2 == 0 {
+                let mult = 10usize.pow(n_digits / 2);
+                let left = val / mult;
+                let right = val % mult;
+                *nc.entry(left).or_default() += count;
+                *nc.entry(right).or_default() += count;
+            } else {
+                *nc.entry(val * 2024).or_default() += count;
+            }
+        }
+        counter = nc;
+    }
+    counter.values().sum()
 }
