0ab16ba49f8b3dd4b323605c69fa90d8caffaa82
J. Fernando Sánchez <balkian@gmail.com>
2024-12-12 10:24:36 +0100

    2024 rust: day12 slight optimization

--- 2024/rust/src/solutions/day12.rs
+++ 2024/rust/src/solutions/day12.rs
@@ -55,12 +55,13 @@ fn changes<T: Eq>(grid: &[Vec<T>], (i, j): (usize, usize), (di, dj): (isize, isi
     underi || underj || pi >= grid.len() || pj >= grid[pi].len() || grid[pi][pj] != grid[i][j]
 }
 
+const DIRS: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 pub fn part1(input: &Input) -> usize {
     let (regions, area) = regions_area(input);
     let mut fences: HashMap<usize, usize> = Default::default();
     for (i, row) in regions.iter().enumerate() {
         for (j, target) in row.iter().enumerate() {
-            for (di, dj) in [(-1, 0), (1, 0), (0, 1), (0, -1)] {
+            for (di, dj) in DIRS {
                 if changes(&regions, (i, j), (di, dj)) {
                     fences.entry(*target).and_modify(|v| *v += 1).or_insert(1);
                 }
@@ -82,19 +83,18 @@ pub fn part2(input: &Input) -> usize {
     let mut fences: HashMap<usize, usize> = Default::default();
     for (i, row) in regions.iter().enumerate() {
         for (j, target) in row.iter().enumerate() {
-            let dirs: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
             let corners = [
-                changes(&regions, (i, j), dirs[0]),
-                changes(&regions, (i, j), dirs[1]),
-                changes(&regions, (i, j), dirs[2]),
-                changes(&regions, (i, j), dirs[3]),
+                changes(&regions, (i, j), DIRS[0]),
+                changes(&regions, (i, j), DIRS[1]),
+                changes(&regions, (i, j), DIRS[2]),
+                changes(&regions, (i, j), DIRS[3]),
             ];
             let f = fences.entry(*target).or_default();
             for pos in 0..4 {
                 let c1 = corners[pos];
                 let c2 = corners[(pos + 1) % 4];
-                let d1 = dirs[pos];
-                let d2 = dirs[(pos + 1) % 4];
+                let d1 = DIRS[pos];
+                let d2 = DIRS[(pos + 1) % 4];
                 let accross = (d1.0 + d2.0, d1.1 + d2.1);
                 if c1 && c2 || (!c1 && !c2 && changes(&regions, (i, j), accross)) {
                     *f += 1;
