accb05fa9bfb328d59e5dce00b7842be4b41b664
J. Fernando Sánchez <balkian@gmail.com>
2024-12-18 08:51:04 +0100

    2024 rust: day 18

---
+++ 2024/rust/src/solutions/day18.rs
@@ -0,0 +1,66 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+type Pos = (usize, usize);
+
+pub fn parse(i: &str) -> Vec<Pos> {
+    i.lines().filter(|line| !line.is_empty())
+        .map(|line| {
+            let mut toks = line.trim().split(",");
+            (toks.next().expect("failed to get first number").parse().expect("first number not a valid number"),
+            toks.next().expect("failed to get second number").parse().expect("first number not a valid number"))
+        }).collect()
+}
+
+const SIZE: usize = 71;
+
+fn exit_until(bytes: usize, i: &[Pos]) -> Option<usize> {
+    let mut dists = [[None; SIZE]; SIZE];
+    let mut blocked = [[false; SIZE]; SIZE];
+
+    for pos in i.iter().take(bytes) {
+        blocked[pos.0][pos.1] = true;
+    }
+
+    let mut heap = BinaryHeap::new();
+    let target = (SIZE-1, SIZE-1);
+    heap.push(Reverse((0, (0, 0))));
+    while let Some(Reverse((cost, pos))) = heap.pop() {
+        if blocked[pos.0][pos.1] {
+            continue;
+        }
+        if dists[pos.0][pos.1].is_some() {
+            continue;
+        }
+        dists[pos.0][pos.1] = Some(cost);
+        if target == pos {
+            return Some(cost);
+        }
+        if (1..SIZE).contains(&pos.0) {
+            heap.push(Reverse((cost+1, (pos.0 - 1, pos.1))));
+        }
+        if (1..SIZE).contains(&pos.1) {
+            heap.push(Reverse((cost+1, (pos.0, pos.1 - 1))));
+        }
+        if (0..SIZE-1).contains(&pos.0) {
+            heap.push(Reverse((cost+1, (pos.0 + 1, pos.1))));
+        }
+        if (0..SIZE-1).contains(&pos.1) {
+            heap.push(Reverse((cost+1, (pos.0, pos.1 + 1))));
+        }
+    }
+    None
+}
+
+pub fn part1(i: &[Pos]) -> usize {
+    exit_until(1024, i).expect("no solution found")
+}
+
+pub fn part2(i: &[Pos]) -> String {
+    for (ix, block) in i.iter().enumerate().skip(1024) {
+        if exit_until(ix+1, i).is_none() {
+            return format!("{},{}", block.0, block.1);
+        }
+    }
+    panic!("no solution found");
+}
