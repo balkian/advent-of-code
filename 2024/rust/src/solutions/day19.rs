f8297dd8cedc3faf43b8e2b40079e446ae0e59d6
J. Fernando Sánchez <balkian@gmail.com>
2024-12-19 18:36:20 +0100

    2024 rust: day19

---
+++ 2024/rust/src/solutions/day19.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+
+#[derive(Debug,Clone)]
+pub struct Problem {
+    tokens: Vec<String>,
+    towels: Vec<String>,
+}
+
+pub fn parse(i: &str) -> Problem {
+    let mut lines = i.lines();
+    let tokens = lines.next().expect("cannot find").trim().split(", ").map(|s| s.to_string()).collect();
+    lines.by_ref().next();
+    let mut towels = vec![];
+    while let Some(towel) = lines.next() {
+        towels.push(towel.trim().to_string());
+    }
+    Problem{tokens, towels}
+}
+
+pub fn matches(i: &str, tokens: &[String]) -> bool {
+    if i.is_empty() {
+        return true;
+    }
+    for t in tokens {
+        if i.starts_with(t) {
+            let (_, newrem) = i.split_at(t.len());
+            if matches(newrem, tokens) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+pub fn count_matches<'a: 'b, 'b>(i: &'a str, tokens: &[String], cache: &'b mut HashMap<&'a str, usize>) -> usize {
+    if i.is_empty() {
+        return 1;
+    }
+    if let Some(already) = cache.get(i) {
+        return *already;
+    }
+    let mut found = 0;
+    for t in tokens {
+        if i.starts_with(t) {
+            let (_, newrem) = i.split_at(t.len());
+            found += count_matches(newrem, tokens, cache);
+        }
+    }
+    cache.insert(i, found);
+    return found;
+}
+
+pub fn part1(p: &Problem) -> usize {
+    let mut possible = vec![];
+    for towel in &p.towels {
+        if matches(towel, &p.tokens) {
+            possible.push(towel);
+        }
+    }
+    possible.len()
+}
+
+pub fn part2(p: &Problem) -> usize {
+    let mut possible = 0;
+    let cache = &mut HashMap::new();
+    for towel in &p.towels {
+        possible += count_matches(towel, &p.tokens, cache);
+    }
+    possible
+}
