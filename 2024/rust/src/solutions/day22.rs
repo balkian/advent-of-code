7ec3cb01fe52acc91b1263624be7ca589989246b
J. Fernando Sánchez <balkian@gmail.com>
2024-12-23 15:16:51 +0100

    2024 rust: day 22 optimization

--- 2024/rust/src/solutions/day22.rs
+++ 2024/rust/src/solutions/day22.rs
@@ -31,8 +31,9 @@ pub fn part1(i: &[usize]) -> usize {
 }
 
 pub fn part2(i: &[usize]) -> usize {
-    let mut seen: HashMap<[isize; 4], HashMap<usize, usize>> = HashMap::with_capacity(20 * i.len());
-    for (j, secret) in i.iter().enumerate() {
+    let mut seen: HashMap<[isize; 4], usize> = HashMap::with_capacity(20 * i.len());
+    let mut seen_now: HashMap<[isize; 4], usize> = HashMap::with_capacity(2000);
+    for secret in i {
         let mut secret = *secret;
         let mut window = [0isize; 4];
         let mut last_val = 0isize;
@@ -46,16 +47,13 @@ pub fn part2(i: &[usize]) -> usize {
             let new_val = (secret % 10) as isize;
             window[3] = new_val - last_val;
             last_val = new_val;
-            seen.entry(window.clone())
-                .or_default()
-                .entry(j)
-                .or_insert(new_val as usize);
+            seen_now.entry(window).or_insert(new_val as usize);
             secret = evolve(secret);
             window.rotate_left(1);
         }
+        for (w, t) in seen_now.drain() {
+            *seen.entry(w).or_default() += t;
+        }
     }
-    seen.values()
-        .map(|v| v.iter().map(|(_j, v)| v).sum::<usize>())
-        .max()
-        .expect("there should be a maximum")
+    *seen.values().max().expect("there should be a maximum")
 }
