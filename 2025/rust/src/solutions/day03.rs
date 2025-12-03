5245175a22a6ee3b2295b40c3a6f8cba28bf53c2
J. Fernando Sánchez <balkian@gmail.com>
2025-12-03 09:46:09 +0100

    2025 rust: day 03 part 2

--- 2025/rust/src/solutions/day03.rs
+++ 2025/rust/src/solutions/day03.rs
@@ -8,16 +8,15 @@ pub fn parse(input: &str) -> Vec<Vec<usize>> {
     }).collect()
 }
 
-pub fn part1(packs: &[Vec<usize>]) -> usize {
-
-    packs.iter()
-        .map(|pack| {
-            let mut a = pack[0];
-            if pack.len() == 1 {
+pub fn part1(banks: &[Vec<usize>]) -> usize {
+    banks.iter()
+        .map(|bank| {
+            let mut a = bank[0];
+            if bank.len() == 1 {
                 return a;
             }
             let mut max = a;
-            for b in &pack[1..pack.len()] {
+            for b in &bank[1..bank.len()] {
                 max = max.max(a * 10 + b);
                 if *b > a {
                     a = *b;
@@ -27,6 +26,32 @@ pub fn part1(packs: &[Vec<usize>]) -> usize {
         }).sum()
 }
 
-pub fn part2(i: &[Vec<usize>]) -> usize {
-    todo!()
+fn joltage(positions: &[usize], batteries: &[usize]) -> usize {
+    positions.iter().fold(0, |acc, jolts| {
+        acc * 10 + batteries[*jolts]
+    })
+}
+
+pub fn part2(banks: &[Vec<usize>]) -> usize {
+    banks.iter()
+        .map(|bank| {
+            let mut chosen: [usize; 12] = std::array::from_fn(|i| i);
+            for start in 1..=bank.len()-chosen.len() {
+                let mut left = chosen[0];
+                let mut right = start;
+
+                for cur in chosen.iter_mut() {
+                    *cur = left;
+                    for option in left+1..=right {
+                        if bank[option] > bank[*cur] {
+                            *cur = option;
+                        }
+                    }
+                    left = *cur + 1;
+                    right += 1;
+                }
+            }
+            //let chosenbank: Vec<usize> = chosen.iter().map(|c| bank[*c]).collect();
+            joltage(&chosen, bank)
+        }).sum()
 }
