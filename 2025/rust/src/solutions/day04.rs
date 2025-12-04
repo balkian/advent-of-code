79958b4e8d4eabb4bfa76292fcbf3cd6349ada0a
J. Fernando Sánchez <balkian@gmail.com>
2025-12-04 15:10:56 +0100

    2025 rust: day 04 mini optimization

--- 2025/rust/src/solutions/day04.rs
+++ 2025/rust/src/solutions/day04.rs
@@ -1,28 +1,32 @@
 pub fn parse(input: &str) -> Vec<Vec<bool>> {
-    input.lines()
+    input
+        .lines()
         .filter(|line| !line.is_empty())
-        .map(|line| {
-            line.trim().chars().map(|c| c == '@').collect()
-        }).collect()
+        .map(|line| line.trim().chars().map(|c| c == '@').collect())
+        .collect()
 }
 
-pub fn neighbors((x, y): (usize, usize), (lenx, leny): (usize, usize)) -> impl Iterator<Item=(usize, usize)> {
-    let xrange = x.saturating_sub(1)..(x+2).min(lenx);
-    let yrange = y.saturating_sub(1)..(y+2).min(leny);
-    xrange.flat_map(move |nx| {
-        yrange.clone().map(move |ny| (nx, ny))
-    }).filter(move |(nx, ny)| *nx != x || *ny != y)
+pub fn neighbors(
+    (x, y): (usize, usize),
+    (lenx, leny): (usize, usize),
+) -> impl Iterator<Item = (usize, usize)> {
+    let xrange = x.saturating_sub(1)..(x + 2).min(lenx);
+    let yrange = y.saturating_sub(1)..(y + 2).min(leny);
+    xrange
+        .flat_map(move |nx| yrange.clone().map(move |ny| (nx, ny)))
+        .filter(move |(nx, ny)| *nx != x || *ny != y)
 }
 
 pub fn part1(map: &[Vec<bool>]) -> usize {
-
     let mut total = 0;
     for (i, row) in map.iter().enumerate() {
         for (j, cell) in row.iter().enumerate() {
             if !cell {
                 continue;
             }
-            let filled = neighbors((i, j), (map.len(), row.len())).filter(|(nx, ny)| map[*nx][*ny]).count();
+            let filled = neighbors((i, j), (map.len(), row.len()))
+                .filter(|(nx, ny)| map[*nx][*ny])
+                .count();
             if filled < 4 {
                 //eprintln!("({i}, {j}) {filled}");
                 total += 1;
@@ -35,20 +39,25 @@ pub fn part1(map: &[Vec<bool>]) -> usize {
 pub fn part2(map: &[Vec<bool>]) -> usize {
     let mut total = 0;
     let mut map: Vec<Vec<bool>> = map.to_vec();
-    let mut nmap: Vec<Vec<bool>>;
+    let ml = map.len();
+    let rl = if ml > 0 { map[0].len() } else { 0 };
     loop {
         let mut removed = 0;
-        nmap = map.clone();
-        for (i, row) in map.iter().enumerate() {
-            for (j, cell) in row.iter().enumerate() {
+        for i in 0..ml {
+            for j in 0..rl {
+                let cell = map[i][j];
                 if !cell {
                     continue;
                 }
-                let filled = neighbors((i, j), (map.len(), row.len())).filter(|(nx, ny)| map[*nx][*ny]).count();
-                if filled < 4 {
+                if neighbors((i, j), (map.len(), rl))
+                    .filter(|(nx, ny)| map[*nx][*ny])
+                    .take(4)
+                    .count()
+                    < 4
+                {
                     //eprintln!("({i}, {j}) {filled}");
                     removed += 1;
-                    nmap[i][j] = false;
+                    map[i][j] = false;
                 }
             }
         }
@@ -56,7 +65,6 @@ pub fn part2(map: &[Vec<bool>]) -> usize {
             break;
         }
         total += removed;
-        map = nmap;
     }
     total
 }
