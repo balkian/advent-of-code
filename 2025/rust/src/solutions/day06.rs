0bebaa0c6b9249720856ef9f6997152582d7b5aa
J. Fernando Sánchez <balkian@gmail.com>
2025-12-06 16:17:12 +0100

    2025 rust: day 06 clippy + fmt

--- 2025/rust/src/solutions/day06.rs
+++ 2025/rust/src/solutions/day06.rs
@@ -2,23 +2,18 @@
 pub enum Operation {
     Sum,
     Multiply,
-
 }
 
 impl Operation {
     fn reduce(&self, nums: &[usize]) -> usize {
         match self {
-            Operation::Sum => {
-                nums.iter().sum()
-            }
-            Operation::Multiply => {
-                nums.iter().product()
-            }
+            Operation::Sum => nums.iter().sum(),
+            Operation::Multiply => nums.iter().product(),
         }
     }
 }
 
-fn convert_ceph<'a>(nums: impl IntoIterator<Item=&'a str>) -> Vec<usize> {
+fn convert_ceph<'a>(nums: impl IntoIterator<Item = &'a str>) -> Vec<usize> {
     let mut result = vec![];
 
     for num in nums.into_iter() {
@@ -26,7 +21,7 @@ fn convert_ceph<'a>(nums: impl IntoIterator<Item=&'a str>) -> Vec<usize> {
             result.push(0);
         }
         for (res, digit) in result.iter_mut().zip(num.chars()) {
-            let Some(this) = (digit as char).to_digit(10) else {
+            let Some(this) = digit.to_digit(10) else {
                 continue;
             };
             *res = *res * 10 + this as usize;
@@ -35,7 +30,7 @@ fn convert_ceph<'a>(nums: impl IntoIterator<Item=&'a str>) -> Vec<usize> {
     result
 }
 
-type Input<'a> = (Vec<Vec<&'a str>>,Vec<Operation>);
+type Input<'a> = (Vec<Vec<&'a str>>, Vec<Operation>);
 
 pub fn parse(i: &str) -> &str {
     i
@@ -54,20 +49,22 @@ pub fn parse_ceph<'a>(i: &'a str) -> Input<'a> {
             }
         }
     }
-    let sizes: Vec<_> = separator.iter()
+    let sizes: Vec<_> = separator
+        .iter()
         .enumerate()
         .filter_map(|(ix, val)| val.then_some(ix))
-        .scan(0, |last,  b| {
+        .scan(0, |last, b| {
             let size = b - *last;
             *last = b + 1;
             Some(size)
-        }).collect();
+        })
+        .collect();
 
     let split_ceph = move |line: &'a str| {
         let mut chunks = vec![];
         let mut line = line;
         for size in sizes.iter() {
-            let (chunk, nxt) = line.split_at(*size); 
+            let (chunk, nxt) = line.split_at(*size);
             chunks.push(chunk);
             line = &nxt[1..];
         }
@@ -75,18 +72,25 @@ pub fn parse_ceph<'a>(i: &'a str) -> Input<'a> {
         chunks
     };
 
-    let mut lines = i.lines()
+    let mut lines = i
+        .lines()
         .rev()
         .filter(|line| !line.is_empty())
         .map(split_ceph);
-    let operations: Vec<_> = lines.by_ref().next().expect("could not get operations").into_iter().map(|chunk| {
-        let op = chunk.trim();
-        match op {
-            "*" => Operation::Multiply,
-            "+" => Operation::Sum,
-            _ => panic!("unknown operation {op}")
-        }
-    }).collect();
+    let operations: Vec<_> = lines
+        .by_ref()
+        .next()
+        .expect("could not get operations")
+        .into_iter()
+        .map(|chunk| {
+            let op = chunk.trim();
+            match op {
+                "*" => Operation::Multiply,
+                "+" => Operation::Sum,
+                _ => panic!("unknown operation {op}"),
+            }
+        })
+        .collect();
     let mut cols = vec![];
 
     for row in lines.rev() {
@@ -102,15 +106,22 @@ pub fn parse_ceph<'a>(i: &'a str) -> Input<'a> {
 
 pub fn part1(i: &str) -> usize {
     let (cols, ops) = parse_ceph(i);
-    cols.iter().zip(ops).map(|(col, op)| {
-        let col: Vec<_> = col.iter().map(|cell| cell.trim().parse::<usize>().expect("could not parse int")).collect();
-        op.reduce(&col)
-    }).sum()
+    cols.iter()
+        .zip(ops)
+        .map(|(col, op)| {
+            let col: Vec<_> = col
+                .iter()
+                .map(|cell| cell.trim().parse::<usize>().expect("could not parse int"))
+                .collect();
+            op.reduce(&col)
+        })
+        .sum()
 }
 
 pub fn part2(i: &str) -> usize {
     let (cols, ops) = parse_ceph(i);
-    cols.iter().zip(ops).map(|(col, op)| {
-        op.reduce(&convert_ceph(col.into_iter().cloned()))
-    }).sum()
+    cols.iter()
+        .zip(ops)
+        .map(|(col, op)| op.reduce(&convert_ceph(col.iter().cloned())))
+        .sum()
 }
