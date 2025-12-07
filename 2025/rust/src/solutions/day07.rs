daa91e22512f4b1a0f5cf220d92c2cade1e5ef6a
J. Fernando Sánchez <balkian@gmail.com>
2025-12-07 17:46:36 +0100

    2025 rust: day 07 fmt

--- 2025/rust/src/solutions/day07.rs
+++ 2025/rust/src/solutions/day07.rs
@@ -1,40 +1,52 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 
 type Pos = (isize, isize);
 type Input = (Pos, HashSet<Pos>);
 
 pub fn parse(i: &str) -> Input {
-    let (start, splitters) = i.lines().enumerate().flat_map(|(ix, line)| {
-        line.chars().enumerate().map(move |(jx, cell)| ((ix as isize, jx as isize), cell))
-        }).fold((None, HashSet::new()), |(ref mut start, mut splitters), (pos, c)| {
-            match c {
-                '^'  => {
-                    splitters.insert(pos);
+    let (start, splitters) = i
+        .lines()
+        .enumerate()
+        .flat_map(|(ix, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(jx, cell)| ((ix as isize, jx as isize), cell))
+        })
+        .fold(
+            (None, HashSet::new()),
+            |(ref mut start, mut splitters), (pos, c)| {
+                match c {
+                    '^' => {
+                        splitters.insert(pos);
+                    }
+                    'S' => *start = Some(pos),
+                    _ => {}
                 }
-                'S' => {
-                    *start = Some(pos)
-                }
-                _ => {
-                }
-            }
-            (*start, splitters)
-
-    });
+                (*start, splitters)
+            },
+        );
     (start.expect("start not found"), splitters)
 }
 
 pub fn part1((start, splitters): &Input) -> usize {
     let mut beams = HashSet::new();
     beams.insert(start.1);
-    let maxlevel: isize = splitters.iter().map(|(level, _)| level).max().copied().unwrap_or_default();
+    let maxlevel: isize = splitters
+        .iter()
+        .map(|(level, _)| level)
+        .max()
+        .copied()
+        .unwrap_or_default();
 
     let mut collisions = 0;
     for level in start.0..=maxlevel {
-        let split: Vec<_> = beams.extract_if(|x| splitters.contains(&(level, *x))).collect();
+        let split: Vec<_> = beams
+            .extract_if(|x| splitters.contains(&(level, *x)))
+            .collect();
         for x in split {
             collisions += 1;
-            beams.insert(x-1);
-            beams.insert(x+1);
+            beams.insert(x - 1);
+            beams.insert(x + 1);
         }
     }
     collisions
@@ -43,13 +55,20 @@ pub fn part1((start, splitters): &Input) -> usize {
 pub fn part2((start, splitters): &Input) -> usize {
     let mut beams = HashMap::new();
     beams.insert(start.1, 1);
-    let maxlevel: isize = splitters.iter().map(|(level, _)| level).max().copied().unwrap_or_default();
+    let maxlevel: isize = splitters
+        .iter()
+        .map(|(level, _)| level)
+        .max()
+        .copied()
+        .unwrap_or_default();
 
     for level in start.0..=maxlevel {
-        let split: Vec<_> = beams.extract_if(|x, _| splitters.contains(&(level, *x))).collect();
+        let split: Vec<_> = beams
+            .extract_if(|x, _| splitters.contains(&(level, *x)))
+            .collect();
         for (x, val) in split {
-            *beams.entry(x-1).or_default() += val;
-            *beams.entry(x+1).or_default() += val;
+            *beams.entry(x - 1).or_default() += val;
+            *beams.entry(x + 1).or_default() += val;
         }
     }
     beams.values().sum()
