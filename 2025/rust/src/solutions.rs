b306862432c1638d28fae10b0d6baa73266e0108
J. Fernando Sánchez <balkian@gmail.com>
2025-12-07 17:41:06 +0100

    2025 rust: day 07 part 1

--- 2025/rust/src/solutions.rs
+++ 2025/rust/src/solutions.rs
@@ -1,3 +1,3 @@
 use aoc_utils::aoc_main;
 
-aoc_main!(day01;day02;day03;day04;day05;day06;);
+aoc_main!(day01;day02;day03;day04;day05;day06;day07;);
---
+++ 2025/rust/src/solutions/day07.rs
@@ -0,0 +1,45 @@
+use std::collections::HashSet;
+
+type Pos = (isize, isize);
+type Input = (Pos, HashSet<Pos>);
+
+pub fn parse(i: &str) -> Input {
+    let (start, splitters) = i.lines().enumerate().flat_map(|(ix, line)| {
+        line.chars().enumerate().map(move |(jx, cell)| ((ix as isize, jx as isize), cell))
+        }).fold((None, HashSet::new()), |(ref mut start, mut splitters), (pos, c)| {
+            match c {
+                '^'  => {
+                    splitters.insert(pos);
+                }
+                'S' => {
+                    *start = Some(pos)
+                }
+                _ => {
+                }
+            }
+            (*start, splitters)
+
+    });
+    (start.expect("start not found"), splitters)
+}
+
+pub fn part1((start, splitters): &Input) -> usize {
+    let mut beams = HashSet::new();
+    beams.insert(start.1);
+    let maxlevel: isize = splitters.iter().map(|(level, _)| level).max().copied().unwrap_or_default();
+
+    let mut collisions = 0;
+    for level in start.0..=maxlevel {
+        let split: Vec<_> = beams.extract_if(|x| splitters.contains(&(level, *x))).collect();
+        for x in split {
+            collisions += 1;
+            beams.insert(x-1);
+            beams.insert(x+1);
+        }
+    }
+    collisions
+}
+
+pub fn part2((start, splitters): &Input) -> usize {
+    todo!()
+}
